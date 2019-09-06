c71afdc779a5bfd1d0608545c04284d08c111797
pashaish <pashaishakov@gmail.com>
2019-09-06 17:48:38 +0700

    create common import

--- index.ts
+++ index.ts
@@ -1,17 +1,4 @@
+import Stream from "./src/Stream/Stream";
+import StreamCallback from "./src/StreamCallback/StreamCallback";
 
-// import Stream from "./class/Stream/Stream";
-
-// const stream = new Stream<number>();
-
-// stream.listen((data) => {
-//     if (data === 1) {
-//         return false;
-//     }
-//     console.debug(data);
-//     return true;
-// });
-
-// stream.add(25534);
-// stream.add(52.26);
-// stream.add(1);
-// stream.add(243);
+export { Stream, StreamCallback };
